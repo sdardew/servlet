0b20aca131c8812e72539d0173e33051586ac7cb
sdardew <sdardew@gmail.com>
2021-12-20 14:26:10 +0900

    Member Management Web Application Requirements

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance(); // Singleton
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given: 이런 상황에서
+        Member member = new Member("hello", 20);
+
+        // when: 이걸 했을 때
+        Member saveMember = memberRepository.save(member);
+
+        // then: 이런 결과가 나와야 한다
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
